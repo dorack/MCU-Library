7f39cf63bec91664d7338d1ce7954ef80e6e1932
Stanley Handschuh <stanleyhandschuh@yahoo.com>
2014-07-20 12:19:05 -0500

    Create version.h

---
+++ version.h
@@ -0,0 +1,28 @@
+/**
+ * @(#)File:           $RCSfile: IIC_Gyroscope.cpp $
+ * @(#)Version:        $Revision: 0.1.0 $
+ * @(#)Last changed:   $Date: 2014/07/20 12:00:00 $
+ * @(#)Purpose:        Version tracking file
+ * @(#)Author:         S Handschuh
+ * @(#)Copyright:      (C) 2014
+ * @(#)License:        MIT
+ */
+ 
+#include "string.h"
+const String VERSION = "v 0.2.0"
+
+/**
+ * Version 0.2.0
+ *
+ * IIC Namespace
+ * IIC, Accelerometer, Compass, Gyroscope, and Temperature classes
+ * Examples of Accelerometer, Compas, Gyroscope, and Temperature classes
+ * Support for ADXL345, HMC5883L, L3G4200D, and BMP085 added
+ */
+ 
+/**
+ * Version 0.1.0
+ *
+ * DeviceDrivers Namespace
+ * HBridge, DualHBridge and QuadHBridge classes
+ */
